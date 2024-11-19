5e7ace2edca6f4862d761c43f48c67a4e4352846
Jose Vines <jose.vines@ug.uchile.cl>
2024-11-19 17:34:43 -0300

    Added CORS policy.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,12 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .database import engine, Base
 from .database.utils import lifespan
 from .routers import everyday
 
 
+origins = [
+    "http://localhost:5173",
+    "http://localhost:5174"
+]
+
+
 def create_app():
     Base.metadata.create_all(bind=engine)
     app = FastAPI(lifespan=lifespan)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*']
+    )
     app.include_router(everyday.router)
     
     return app
