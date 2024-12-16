3e136c354eaf4a0cf59f38a4458ddc5faa1e2988
Jose Vines <jose.vines.l@gmail.com>
2024-12-15 23:21:20 -0300

    Added more exceptions to the CORS policy.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -7,7 +7,9 @@ from .routers import everyday
 
 origins = [
     "http://localhost:5173",
-    "http://localhost:5174"
+    "http://localhost:5174",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:5174",
 ]
 
 
